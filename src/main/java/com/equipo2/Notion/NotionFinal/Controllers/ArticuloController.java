22faccabb6038419781a2c693ebb74b1285c2515
Josema <josema.saborido@gmail.com>
2021-11-10 19:19:35 +0100

    Subida Inicial del proyecto

--- src/main/java/com/equipo2/Notion/NotionFinal/Controllers/ArticuloController.java
+++ src/main/java/com/equipo2/Notion/NotionFinal/Controllers/ArticuloController.java
@@ -1,7 +1,6 @@
 package com.equipo2.Notion.NotionFinal.Controllers;
 
-import com.equipo2.Notion.NotionFinal.Entities.Articulo;
-import com.equipo2.Notion.NotionFinal.Services.ArticuloService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
